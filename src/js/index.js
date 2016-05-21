71af5f1735bf62a83bc061dbab5ebdc793cb2997
Bastian Schade <bastianschadeberlin@googlemail.com>
2016-05-21 09:33:02 +0200

    Add and delete entry items

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,50 @@
 import Horizon from '@horizon/client';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Entry from './entry';
 
 const horizon = Horizon({ host: 'localhost:8181' });
-
-horizon.onReady(() => document.querySelector('h1').innerHTML = 'Horizon is ready');
-
+const messages = horizon('messages');
 horizon.connect();
+
+const handleDelClick = (id) => {
+  messages.remove(id);
+}
+
+const Bar = props => {
+  const messages = props.messages.map((message, index) =>
+    <Entry
+      key={index}
+      handleDelClick={handleDelClick}
+      message={message}
+    />
+  );
+  return (
+    <div>{messages}</div>
+  );
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {messages: []};
+    messages.watch().subscribe(items => this.setState({ messages: [...items]}));
+  }
+
+  handleClick = (ev) => {
+    ev.preventDefault();
+    messages.store({ time: new Date(), copy: 'n/a' });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Add</button>
+        <Bar messages={this.state.messages} />
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
